2a8dd7dca516a28d7e76e077fb3e4fec783af8d9
theyasaman <shirdastyasaman@gmail.com>
2025-03-11 15:28:38 -0400

    Update main.tf
    added the first draft of the deployed VM instance ( need to change the image type being used.

--- tf/modules/vm/main.tf
+++ tf/modules/vm/main.tf
@@ -1,1 +1,24 @@
+#source https://cloud.google.com/vpc/docs/create-use-multiple-interfaces#creating_virtual_machine_instances_with_multiple_network_interfaces
+# use 
 
+
+resource "google_compute_instance" "default" {
+  project      = var.project_id # Replace with your project ID in quotes
+  zone         = "us-central1-b"
+  name         = "backend-instance"
+  machine_type = "e2-medium"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    subnetwork = var.subnet_1 # Replace with self link to a subnetwork in quotes
+    network_ip = "10.0.0.14"
+  }
+  network_interface {
+    subnetwork = var.subnet_2 # Replace with self link to a subnetwork in quotes
+    network_ip = "10.10.20.14"
+  }
+  can_ip_forward = true
+}
